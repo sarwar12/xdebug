10ffe13d573adcdba6e49e5edcb6e6e7264f0b70
sarwar12 <sarwarkhan.nahin@gmail.com>
2024-10-25 16:38:45 +0600

    2nd

--- profile.php
+++ profile.php
@@ -1,14 +1,18 @@
 <?php 
 for($i = 0; $i<10; $i++){
     $result = getResult($i);
-    echo "==Displaying Result== \n";
+    echo "==Displaying Result ==<br>";
     displayResult($i, $result);
 }
 
 function getResult($i){
+    usleep(500);
     return $i % 2 == 0 ? 'even': 'odd';
 }
 
 function displayResult($i, $result){
+    usleep(1000);
     printf("%d is %s<br>", $i, $result);
 }
+
+
